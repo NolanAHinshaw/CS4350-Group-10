6c012ca2d9ec72f8ce87c535646e5455fe2bb8e2
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-19 14:07:47 -0400

    Made global changes to background and font type; adjusted responsive design on both the registration page and navigation bar (unwanted horizontal scrolling resolved)

--- studentpulse-ui/src/Pages/Registration Page/RegistrationPage.jsx
+++ studentpulse-ui/src/Pages/Registration Page/RegistrationPage.jsx
@@ -1,48 +1,54 @@
 import './RegistrationPage.css';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import heroImage from '../../Images/registration-hero.png';
-import PulseLogo from '../../Images/StudentPulseLogo.svg';
+import PulseLogo from '../../Images/LogoVector.svg';
 
-export default function RegistrationPage(){
+function RegistrationPage(){
     return(
         <div className='register-page'>
             <div className='hero-image'>
-                <img src={ heroImage } alt='Registration Page Hero'/>
+              {/* <img src={ heroImage } alt='Registration Page Hero'/> */}
+              <h2></h2>
             </div>
 
-
             <div className='register-page-content'>
-                <img src={ PulseLogo } alt='Student Pulse Logo'></img>
+                <img src={ PulseLogo } alt='Student Pulse Logo' className='logo-vector'></img>
                 <h1>Create Your Account</h1>
+                <p>Add some kind of call to action message</p>
 
                 <form action='' className='register-form'>
                     <div className='name-field'>
                         <div className='input-field-section'>
                             <label>First Name</label>
-                            <input type='name' placeholder='Enter first name' />
+                            <input type='name' placeholder='Enter first name' required/>
                         </div>
                         <div className='input-field-section'>
                             <label>Last Name</label>
-                            <input type='name' placeholder='Enter last name' />
+                            <input type='name' placeholder='Enter last name' required/>
                         </div>
                     </div>
                     <div className='input-field-section'>
                         <label>Email</label>
-                        <input type='email' placeholder='Enter email'/>
+                        <input type='email' placeholder='Enter email' required/>
                     </div>
                     <div className='input-field-section'>
                         <label>Password</label>
-                        <input type='password' placeholder='Enter password' />
+                        <input type='password' placeholder='Enter password' required/>
                     </div>
                     <div className='input-field-section'>
                         <label>Confirm Password</label>
-                        <input type='password' placeholder='Enter password again' />
+                        <input type='password' placeholder='Enter password again' required/>
                     </div>
                 </form>
 
+
                 <button className='register-page-button'> SIGN UP </button>
-                <p>Already Have An Account? <Link to='/login'>Sign in </Link></p>
+                <p>Already Have An Account? <Link to='/login' className='sign-in-link'>Sign in </Link></p>
             </div>
         </div>
     );
 }
+
+
+export default React.memo(RegistrationPage);
