3dd26411e50d9fc13651689fc77d200e5d631729
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-19 15:23:27 -0400

    Cleaned up code and comments in registration page css and jsx file

--- studentpulse-ui/src/Pages/Registration Page/RegistrationPage.jsx
+++ studentpulse-ui/src/Pages/Registration Page/RegistrationPage.jsx
@@ -1,10 +1,13 @@
+// IMPORTS ---------------------------------------------------------------------------------------------------------------
 import './RegistrationPage.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RegistrationForm from './RegistrationForm';
 import PulseLogo from '../../Images/LogoVector.svg';
 import GradCapIcon from '../../Images/grad-cap.svg';
-import RegistrationForm from './RegistrationForm';
 
+
+// MAIN REGISTER PAGE FUNCTION -------------------------------------------------------------------------------------------------
 function RegistrationPage(){
     return(
         <div className='register-page'>
@@ -23,7 +26,7 @@ function RegistrationPage(){
                 <RegistrationForm />
 
                 <button className='register-page-button'> SIGN UP </button>
-                <p>Already Have An Account? <Link to='/login' className='sign-in-link'>Sign in </Link></p>
+                <p>Already Have An Account? <Link to='/login' className='sign-in-link'> Sign in </Link></p>
             </div>
         </div>
     );
