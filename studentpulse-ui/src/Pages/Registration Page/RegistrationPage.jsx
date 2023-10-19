6f48b9996dd5acb562f1bf02fb1deb27183218d9
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-19 15:16:21 -0400

    Added final design pushes to registration page; Moved Registration input fields into separate form to implement functionality later on without the clutter

--- studentpulse-ui/src/Pages/Registration Page/RegistrationPage.jsx
+++ studentpulse-ui/src/Pages/Registration Page/RegistrationPage.jsx
@@ -1,47 +1,26 @@
 import './RegistrationPage.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import heroImage from '../../Images/registration-hero.png';
 import PulseLogo from '../../Images/LogoVector.svg';
+import GradCapIcon from '../../Images/grad-cap.svg';
+import RegistrationForm from './RegistrationForm';
 
 function RegistrationPage(){
     return(
         <div className='register-page'>
             <div className='hero-image'>
-              {/* <img src={ heroImage } alt='Registration Page Hero'/> */}
-              <h2></h2>
+                <div className='hero-text'>
+                    <h2> Boost Your Academic Excellence With Student Pulse </h2>
+                    <p> Prioritizing student productivity, collaboration on real-time feedback, and personalized recommendations !</p>
+                    <img src={  GradCapIcon } alt='Graduation Cap Icon'/>
+                </div>
             </div>
 
             <div className='register-page-content'>
                 <img src={ PulseLogo } alt='Student Pulse Logo' className='logo-vector'></img>
                 <h1>Create Your Account</h1>
-                <p>Add some kind of call to action message</p>
 
-                <form action='' className='register-form'>
-                    <div className='name-field'>
-                        <div className='input-field-section'>
-                            <label>First Name</label>
-                            <input type='name' placeholder='Enter first name' required/>
-                        </div>
-                        <div className='input-field-section'>
-                            <label>Last Name</label>
-                            <input type='name' placeholder='Enter last name' required/>
-                        </div>
-                    </div>
-                    <div className='input-field-section'>
-                        <label>Email</label>
-                        <input type='email' placeholder='Enter email' required/>
-                    </div>
-                    <div className='input-field-section'>
-                        <label>Password</label>
-                        <input type='password' placeholder='Enter password' required/>
-                    </div>
-                    <div className='input-field-section'>
-                        <label>Confirm Password</label>
-                        <input type='password' placeholder='Enter password again' required/>
-                    </div>
-                </form>
-
+                <RegistrationForm />
 
                 <button className='register-page-button'> SIGN UP </button>
                 <p>Already Have An Account? <Link to='/login' className='sign-in-link'>Sign in </Link></p>
