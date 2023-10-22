e5314ea5d486a52ff953a4b3c581d9fc1eff5837
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-22 17:29:14 -0400

    Created landing page hero and darkened the color of nav bar

--- studentpulse-ui/src/Pages/Landing Page/LandingPage.jsx
+++ studentpulse-ui/src/Pages/Landing Page/LandingPage.jsx
@@ -1,9 +1,21 @@
 import './LandingPage.css';
+import { Link } from 'react-router-dom';
 
 export default function LandingPage(){
     return(
         <div className='Landing'>
-            THIS IS THE LANDING PAGE
+            <div className='landing-hero'>
+                <div className='landing-hero-text'>
+                    <h2>Maximize Your Academic Excellence With Student Pulse</h2>
+                    <p>
+                        Get organized For Success and boost your Productivity today! 
+                        Explore all The features Student Pulse has to offer and explore 
+                        real-time course feedback!
+                    </p>
+                    <Link to='/register' className='landing-action-button'> Get Started Now! </Link>
+                </div>
+            </div>
+            <h1>hello world</h1>
         </div>
     );
 }
